a6f15d5adcf5f881456ae881339f5980687bb819
Ayemere <odiadavid2@gmail.com>
2022-01-31 00:03:17 +0100

    part solution

--- MaxSubArray.playground/Contents.swift
+++ MaxSubArray.playground/Contents.swift
@@ -3,45 +3,45 @@ import UIKit
 
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        return maxSub(nums)
+        return maxSub(nums,0,nums.count - 1)
     }
     
-    func maxSub(_ nums:[Int]) -> Int {
-        
-        let L = 0
-        let R = nums.count
-        
-        if L == R { return nums[0] }
-        
+    func maxSub(_ nums:[Int], _ L:Int, _ R:Int) -> Int {
+        if L == R { return nums[L] }
         let m = L + (R - L) / 2
-        
-        let l = maxSub(nums)
-        let r = maxSub(nums)
+
+                
+        let l = maxSub(nums,L,m-1)
+        let r = maxSub(nums,m+1,R)
         let c = maxCrossSubArray(nums,L,m,R)
         return max(l,r,c)
     }
+    
     
     func maxCrossSubArray(_ nums:[Int], _ L:Int, _ m:Int, _ R: Int) -> Int {
         var leftSum = 0
         var rightSum = 0
         var curSum = 0
-        for i in 0..<m {
+        for i in (0..<m).reversed() {
             curSum += nums[i]
             leftSum = max(curSum,leftSum)
+            print("leftSum is ",leftSum)
         }
-        
+        curSum = 0
         for i in m..<R {
             curSum += nums[i]
             rightSum = max(curSum,rightSum)
+            print("rightSum is ",rightSum)
         }
-        return max(leftSum,rightSum)
+        return (leftSum + rightSum)
     }
+    
 }
 
 
 let pro = Solution()
 
-let res = pro.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+let res = pro.maxSubArray([5,4,-1,7,8])
 /*
  for val1 in 0..<nums.count {
      nextLarge = nums[val1]
@@ -60,3 +60,29 @@ let res = pro.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
      }
  }
  */
+
+func maxCrossSubArray(_ nums:[Int]) -> Int {
+    var leftSum = 0
+    var rightSum = 0
+    var curSum = 0
+    
+    let L = 0
+    let R = nums.count
+        
+    let m = L + (R - L) / 2
+    
+    for i in (0..<m).reversed() {
+        curSum += nums[i]
+        leftSum = max(curSum,leftSum)
+        print("leftSum is ",leftSum)
+    }
+    curSum = 0
+    for i in m..<R {
+        curSum += nums[i]
+        rightSum = max(curSum,rightSum)
+        print("rightSum is ",rightSum)
+    }
+    return (leftSum + rightSum)
+}
+
+//let result = maxCrossSubArray([-2,1])
