7592c8e271b4ae0f630e72b4ac58e6490c32a155
Ayemere <odiadavid2@gmail.com>
2022-02-04 06:42:39 +0100

    simpler solution

--- AddBinary.playground/Contents.swift
+++ AddBinary.playground/Contents.swift
@@ -13,16 +13,21 @@ func toPower2(power:Int) -> Int {
 //toPower2(power:0)
 
 func toBinary(baseTen:Int) -> String {
-    var temp = baseTen
-    var str = [Int]()
-    while temp >= 1 {
-        let rem = temp % 2
-        str.insert(rem, at:0)
-        temp = temp / 2
-    }
-    var res = ""
-     str.map {res += String($0)}
-    return res
+//    var temp = baseTen
+//    var str = [Int]()
+//    while temp >= 1 {
+//        let rem = temp % 2
+//        str.insert(rem, at:0)
+//        temp = temp / 2
+//    }
+//    var res = ""
+//     str.map {res += String($0)}
+    
+    if baseTen < 2 { return "\(baseTen)"}
+    let divisor = baseTen / 2
+    let remainder = baseTen % 2
+    
+    return toBinary(baseTen: divisor) + "\(remainder)"
 }
 //toBinary(baseTen:21)
 
@@ -47,4 +52,33 @@ func addBinary(_ a: String, _ b: String) -> String {
     return toBinary(baseTen:aSum + bSum)
 }
 
-addBinary("1010","1011")
+
+
+
+func addBinary1(_ a: String, _ b: String) -> String {
+    guard a.count > 0 else { return b }
+    guard b.count > 0 else { return a }
+    
+    let arr = Array(Array(a).reversed())
+    let brr = Array(Array(b).reversed())
+    
+    var output = ""
+    var remainder = 0
+    
+    let count = max(arr.count, brr.count)
+    for i in 0..<count {
+        let val1 = arr.count > i ? Int("\(arr[i])")! : 0
+        let val2 = brr.count > i ? Int("\(brr[i])")! : 0
+        
+        let sum = val1 + val2 + remainder
+        output = "\(sum%2)\(output)"
+        remainder = sum / 2
+    }
+    
+    if remainder == 1 {
+        output = "\(remainder)\(output)"
+    }
+    
+    return output
+}
+addBinary1("1010","1011")
